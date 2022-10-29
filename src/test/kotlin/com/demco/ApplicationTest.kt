29b2136f5ec1fb1916c7f1d99c98aab6d6720378
Nick <ndemco@gmail.com>
2022-10-29 01:28:50 -0400

    Some clean up

--- src/test/kotlin/com/demco/ApplicationTest.kt
+++ src/test/kotlin/com/demco/ApplicationTest.kt
@@ -1,22 +1,11 @@
 package com.demco
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.serialization.kotlinx.json.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.sessions.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.demco.plugins.*
-import io.ktor.client.call.body
 
 class ApplicationTest {
   @Test
